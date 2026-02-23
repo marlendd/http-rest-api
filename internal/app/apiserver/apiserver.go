4ed1560bd1e6dfeef838f7dffa04b9637cd2e993
marlendd <vadim150605@gmail.com>
2026-02-23 19:56:44 +0300

    implemented DB

--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -3,7 +3,9 @@ package apiserver
 import (
 	"io"
 	"net/http"
+
 	"github.com/gorilla/mux"
+	"github.com/marlendd/http-rest-api/internal/app/store"
 	"github.com/sirupsen/logrus"
 )
 
@@ -11,6 +13,7 @@ type APIserver struct{
 	config *Config 
 	logger *logrus.Logger
 	router *mux.Router
+	store *store.Store
 }
 
 func New(config *Config) *APIserver {
@@ -27,6 +30,10 @@ func (s *APIserver) Start() error {
 	}
 
 	s.configureRouter()
+
+	if err := s.configureStore(); err != nil {
+		return err
+	}
 
 	s.logger.Info("starting api server")
 
@@ -46,6 +53,17 @@ func (s *APIserver) configureLogger() error {
 
 func (s *APIserver) configureRouter() {
 	s.router.HandleFunc("/hello", s.handleHello())
+}
+
+func (s *APIserver) configureStore() error {
+	st := store.New(s.config.Store)
+	if err := st.Open(); err != nil {
+		return err
+	}
+
+	s.store = st
+
+	return nil
 }
 
 func (s *APIserver) handleHello() http.HandlerFunc {
