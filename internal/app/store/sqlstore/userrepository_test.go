18b8db7bcec6bee4b0035f0f56e8f3db0577cd46
marlendd <vadim150605@gmail.com>
2026-02-23 22:29:39 +0300

    some refactoring

--- internal/app/store/sqlstore/userrepository_test.go
+++ internal/app/store/sqlstore/userrepository_test.go
@@ -1,29 +1,32 @@
-package store_test
+package sqlstore_test
 
 import (
 	"testing"
 
 	"github.com/marlendd/http-rest-api/internal/app/model"
 	"github.com/marlendd/http-rest-api/internal/app/store"
+	"github.com/marlendd/http-rest-api/internal/app/store/sqlstore"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestUserRepository_Create(t *testing.T) {
-	s, teardown := store.TestStore(t, databaseURL)
+	db, teardown := sqlstore.TestDB(t, databaseURL)
 	defer teardown("users")
 
-	u, err := s.User().Create(model.TestUser(t))
-	assert.NoError(t, err)
+	s := sqlstore.New(db)
+	u := model.TestUser(t)
+	assert.NoError(t, s.User().Create(u))
 	assert.NotNil(t, u)
 }
 
 func TestUserRepository_FindByEmail(t *testing.T) {
-	s, teardown := store.TestStore(t, databaseURL)
+	db, teardown := sqlstore.TestDB(t, databaseURL)
 	defer teardown("users")
 
+	s := sqlstore.New(db)
 	email := "user@example.org"
 	_, err := s.User().FindByEmail(email)
-	assert.Error(t, err)
+	assert.EqualError(t, err, store.ErrRecordNotFound.Error())
 
 	u := model.TestUser(t)
 	u.Email = email
